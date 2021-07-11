0fb269e72350f347a47a173d11fd414bc69225ad
sontiram <sonti.rambhajan@mahindra.com>
2021-07-11 23:43:15 +0530

    add hover

---
+++ src/main/resources/static/js/Script.js
@@ -0,0 +1,57 @@
+$(function() {
+    $('#popper1').hover(function() {
+        $('#pop1').toggle();
+    });
+    $('#popper2').hover(function() {
+        $('#pop2').toggle();
+    });
+    $('#popper3').hover(function() {
+        $('#pop3').toggle();
+    });
+    $('#popper4').hover(function() {
+        $('#pop4').toggle();
+    });
+    $('#popper5').hover(function() {
+        $('#pop5').toggle();
+    });
+    $('#popper6').hover(function() {
+        $('#pop6').toggle();
+    });
+    $('#popper7').hover(function() {
+        $('#pop7').toggle();
+    });
+    $('#popper8').hover(function() {
+        $('#pop8').toggle();
+    });
+    $('#popper9').hover(function() {
+        $('#pop9').toggle();
+    });
+    $('#popper10').hover(function() {
+        $('#pop10').toggle();
+    });
+    $('#popper11').hover(function() {
+        $('#pop11').toggle();
+    });
+    $('#popper12').hover(function() {
+        $('#pop12').toggle();
+    });
+    $('#popper13').hover(function() {
+        $('#pop13').toggle();
+    });
+    $('#popper14').hover(function() {
+        $('#pop14').toggle();
+    });
+    $('#popper15').hover(function() {
+        $('#pop15').toggle();
+    });
+     $('#popper16').hover(function() {
+        $('#pop16').toggle();
+    });
+});
+
+
+
+
+
+
+
