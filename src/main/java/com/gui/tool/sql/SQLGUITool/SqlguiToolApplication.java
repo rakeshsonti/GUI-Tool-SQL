f1e3adbab2f255e43a9ca07d84e557a87cbea5a1
sontiram <sonti.rambhajan@mahindra.com>
2021-07-11 14:33:31 +0530

    first comment

---
+++ src/main/java/com/gui/tool/sql/SQLGUITool/SqlguiToolApplication.java
@@ -0,0 +1,13 @@
+package com.gui.tool.sql.SQLGUITool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlguiToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlguiToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gui/tool/sql/SQLGUITool/SqlguiToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.tool.sql.SQLGUITool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlguiToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
